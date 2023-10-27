5b3a9d530cf07eb0b7410400e6f8ab5396ce2b39
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-10-26 17:02:07 -0700

    Move archive loading view to new file

--- src/App.svelte
+++ src/App.svelte
@@ -1,78 +1,12 @@
 <script>
-  import * as rgssad from "rgssad-wasm";
   import * as zip from "@zip.js/zip.js";
 
-  import FileNode from "./FileNode.js";
-
+  import LoadArchiveView from "./LoadArchiveView.svelte";
   import TreeViewNode from "./TreeViewNode.svelte";
 
   let rootNode = null;
   let fileName = null;
   let activeFileNode = null;
-
-  function handleArchiveInputDrop(event) {
-    const items = event.dataTransfer.items;
-
-    if (!items) {
-      return;
-    }
-
-    if (items.length !== 1) {
-      alert("Expected only one file");
-      return;
-    }
-
-    const item = items[0];
-
-    if (item.kind !== "file") {
-      alert("Only files are supported");
-      return;
-    }
-
-    const file = item.getAsFile();
-
-    loadArchiveFromFile(file).catch(alert);
-  }
-
-  function handleArchiveInputChange(event) {
-    const input = event.target;
-    if (input.files.length !== 1) {
-      alert("Expected only one file");
-      return;
-    }
-
-    const file = input.files[0];
-
-    loadArchiveFromFile(file).catch(alert);
-  }
-
-  async function loadArchiveFromFile(file) {
-    const arrayBuffer = await file.arrayBuffer();
-    const reader = new rgssad.Reader(arrayBuffer);
-
-    const fileTree = new FileNode({
-      path: "",
-      isFile: false,
-    });
-    for (
-      let entry = reader.readEntry();
-      entry != null;
-      entry = reader.readEntry()
-    ) {
-      const fileNode = new FileNode({
-        path: entry.fileName,
-        isFile: true,
-        data: entry.data,
-      });
-
-      fileTree.addChild(fileNode);
-    }
-
-    rootNode = fileTree;
-    fileName = file.name;
-
-    console.log(rootNode);
-  }
 
   async function downloadZip() {
     if (rootNode === null) {
@@ -131,6 +65,11 @@
 
     return null;
   }
+
+  function handleNewRootNode(event) {
+    fileName = event.detail.fileName;
+    rootNode = event.detail.fileNode;
+  }
 </script>
 
 {#if rootNode !== null}
@@ -161,6 +100,7 @@
               class="preview-image"
               src={imgSrc}
               on:load={handlePreviewImageLoad}
+              alt="preview for {activeFileNode.path}"
             />
           {:else}
             <div class="unknown-file-type">
@@ -172,55 +112,10 @@
     </div>
   </div>
 {:else}
-  <div class="archive-input-container">
-    <label
-      for="archive-input"
-      class="archive-input-label"
-      on:dragover|preventDefault
-      on:drop|preventDefault={handleArchiveInputDrop}
-    >
-      Upload Archive
-    </label>
-    <input
-      type="file"
-      id="archive-input"
-      name="archive-input"
-      accept=".rgssad,.rgss2a"
-      on:change={handleArchiveInputChange}
-    />
-  </div>
+  <LoadArchiveView on:load={handleNewRootNode} />
 {/if}
 
 <style>
-  .archive-input-container {
-    background-color: var(--color-2);
-    border-radius: 0.5rem;
-    display: flex;
-    flex-grow: 1;
-    justify-content: center;
-    margin: 0.5rem;
-    padding: 0.5rem;
-  }
-
-  #archive-input {
-    display: none;
-  }
-
-  .archive-input-label {
-    align-items: center;
-    border: 0.5rem dashed white;
-    border-radius: 0.5rem;
-    box-sizing: border-box;
-    color: white;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    font-size: 1.5rem;
-  }
-
   .viewer-container {
     display: flex;
     flex-direction: column;
---
+++ src/LoadArchiveView.svelte
@@ -0,0 +1,121 @@
+<script>
+  import * as rgssad from "rgssad-wasm";
+  import { createEventDispatcher } from "svelte";
+
+  import FileNode from "./FileNode.js";
+
+  const dispatch = createEventDispatcher();
+
+  function handleInputDrop(event) {
+    const items = event.dataTransfer.items;
+
+    if (!items) {
+      return;
+    }
+
+    if (items.length !== 1) {
+      alert("Expected only one file");
+      return;
+    }
+
+    const item = items[0];
+
+    if (item.kind !== "file") {
+      alert("Only files are supported");
+      return;
+    }
+
+    const file = item.getAsFile();
+
+    loadArchiveFromFile(file).catch(alert);
+  }
+
+  function handleInputChange(event) {
+    const input = event.target;
+    if (input.files.length !== 1) {
+      alert("Expected only one file");
+      return;
+    }
+
+    const file = input.files[0];
+
+    loadArchiveFromFile(file).catch(alert);
+  }
+
+  async function loadArchiveFromFile(file) {
+    const arrayBuffer = await file.arrayBuffer();
+    const reader = new rgssad.Reader(arrayBuffer);
+
+    const fileTree = new FileNode({
+      path: "",
+      isFile: false,
+    });
+    for (
+      let entry = reader.readEntry();
+      entry != null;
+      entry = reader.readEntry()
+    ) {
+      const fileNode = new FileNode({
+        path: entry.fileName,
+        isFile: true,
+        data: entry.data,
+      });
+
+      fileTree.addChild(fileNode);
+    }
+
+    dispatch("load", {
+      fileName: file.name,
+      fileNode: fileTree,
+    });
+  }
+</script>
+
+<div class="container">
+  <label
+    for="archive-input"
+    class="input-label"
+    on:dragover|preventDefault
+    on:drop|preventDefault={handleInputDrop}
+  >
+    Upload Archive
+  </label>
+  <input
+    type="file"
+    id="archive-input"
+    name="archive-input"
+    accept=".rgssad,.rgss2a"
+    on:change={handleInputChange}
+  />
+</div>
+
+<style>
+  .container {
+    background-color: var(--color-2);
+    border-radius: 0.5rem;
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    margin: 0.5rem;
+    padding: 0.5rem;
+  }
+
+  #archive-input {
+    display: none;
+  }
+
+  .input-label {
+    align-items: center;
+    border: 0.5rem dashed white;
+    border-radius: 0.5rem;
+    box-sizing: border-box;
+    color: white;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+</style>
